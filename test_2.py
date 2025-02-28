c14e48d62d8abdbc008e228a4eeff6bbf2947352
jaehun-dean <pjh9788@nate.com>
2025-02-28 01:52:45 +0100

    DB connection

--- test_2.py
+++ test_2.py
@@ -1,7 +1,22 @@
-print("2")
+import sqlite3
 
-print("3")
+# Connect to the database (or create it if it doesn't exist)
+conn = sqlite3.connect('NetPy.db')
 
-print("4")
+# Create a cursor
+cursor = conn.cursor()
 
-print("5")
+# Create a table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS devices (
+    ip TEXT PRIMARY KEY,
+    id TEXT,
+    pw TEXT
+)
+""")
+
+# save the changes
+conn.commit()
+
+# Close the connection
+conn.close()
